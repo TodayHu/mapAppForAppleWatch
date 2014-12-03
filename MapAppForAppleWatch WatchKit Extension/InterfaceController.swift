acd3ba4088cfecab4111d6d56f98bc90f5b7d198
Albert Saucedo <albert@Alberts-Mac-mini.local>
2014-12-02 20:41:17 -0600

    Map now shows my location

---
+++ MapAppForAppleWatch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,43 @@
+//
+//  InterfaceController.swift
+//  MapAppForAppleWatch WatchKit Extension
+//
+//  Created by Albert Saucedo on 12/2/14.
+//  Copyright (c) 2014 Albert Saucedo. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    @IBOutlet weak var mainMap: WKInterfaceMap!
+
+    var location = CLLocationCoordinate2D(latitude: 41.793553, longitude: -87.71799199999998)
+    var span = MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+
+    override init(context: AnyObject?) {
+        // Initialize variables here.
+        super.init(context: context)
+
+        mainMap.setCoordinateRegion(MKCoordinateRegion(center: location, span: span))
+        mainMap.addAnnotation(location, withImageNamed: "Home")
+        
+        // Configure interface objects here.
+        NSLog("%@ init", self)
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+        NSLog("%@ will activate", self)
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        NSLog("%@ did deactivate", self)
+        super.didDeactivate()
+    }
+
+}
